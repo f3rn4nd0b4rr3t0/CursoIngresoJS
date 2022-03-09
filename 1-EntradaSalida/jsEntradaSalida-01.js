7c12c42ee7afe23f8381e1088a6e5908ba874a5d
f3rn4nd0b4rr3t0 <fernandobarreto1985@gmail.com>
2022-03-09 14:26:22 -0300

    Nuevo cambio test

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,42 +1,12 @@
-//Ejercicio 1
-//Nos dedicamos a la venta exclusiva de Discos rígidos.
-//Debemos realizar la carga de 5 (cinco) productos, de cada uno debo obtener los siguientes datos:
-//Tipo: (validar "HDD", "SSD" o "SSDM2")
-//Precio: (validar entre 5000 y 18000)
-//Cantidad de unidades: (no puede ser 0 o negativo y no debe superar las 50 Unidades)
-//Marca: (validar “Seagate”, “Western Digital”, “Kingston”)
-//Capacidad: (validar 250Gb, 500Gb, 1Tb, 2Tb)
-//Se debe Informar al usuario lo siguiente:
-//A) Del más barato de los SSD, la cantidad de unidades y marca.
-//B) Del tipo HDD, el de mayor precio, capacidad de almacenamiento y cantidad de unidades disponibles.
-//C) Cuántas unidades de HDD hay en total.
-
+/*
+Fernand Barreto
+Ejercicio 1
+Al presionar el  botón, 
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 
 function mostrar()
 {
-var producto;
-var tipo;
-var marca;
-var capacidad;
-var precios;
-
-
-producto = parseInt(prompt("Ingrese producto"));
-tipo = parseInt(prompt("HDD" + "SSD"  + "SSDM2"));
-marca = parseInt(prompt("Seagate" + "Western Digital" + "Kingston"));
-capacidad = parseInt(prompt("250Gb" + "500Gb" + "1Tb" + "2Tb"));
-precios = parseInt(prompt(precios));
-
-
-while (precios == 5000 < 18000) {
-    if (tipo == 5000){
-        
-    }
-
-
-}
-
-
+	alert("Esto funciona de maravilla");
 }
 
 
