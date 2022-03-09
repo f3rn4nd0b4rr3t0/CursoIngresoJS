a8e349b612fd86221dd5bafd46076f4c20c01992
f3rn4nd0b4rr3t0 <fernandobarreto1985@gmail.com>
2022-03-09 14:32:36 -0300

    Nuevo cambio test

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,11 +1,14 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
 function mostrar()
 {
-var nombre;
-nombre = prompt (" Ingrese un nombre");
-alert ("Hola " + nombre);
+    var nombre;
+
+    nombre = prompt(" Ingrese un nombre");
+
+    alert("Hola " + nombre);
 }
 
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -2,6 +2,7 @@
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
+
 function mostrar()
 {
    var nombreIngresado;
