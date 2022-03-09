af3ede7428b99dead9f42b6a6b7cb67e47326e95
f3rn4nd0b4rr3t0 <fernandobarreto1985@gmail.com>
2022-03-08 23:42:22 -0300

    Nuevo cambio test

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -1,26 +1,62 @@
-/*
-Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
-e informar la suma acumulada y el promedio.
-*/
+
 function mostrar() 
 {
-	var numeroIngresado;
-	var contador;
-	var acumulador;
-	var promedio;
-	var respuesta = "si";
+	var nombre;
+    var edad;
+    var peso;
+    var altura;
+    var flagedadJugadorMasAlto = 0;
+    var edadJugadorMasAlto;
+    var alturaMayor;
+    var flagJugadorMenosPesado = 0;
+    var nombreJugadorMenosPesado;
+    var pesoJugadorMenosPesado;
+    var pesoDelEquipo = 0;
+    var cantidadJugadores = 0;
+    var promedioPesoEquipo;
+    var jugadoresEdad13Y15Altura165Y179 = 0;
 
-	acumulador = 0;
-	contador = 0;
+    for (var i = 1; i <= 11; i++) {
+        nombre = prompt("Ingrese nombre: ").toLowerCase();
 
-	while (respuesta == "si") {
-		numeroIngresado = parseInt(prompt("Ingrese un número"));
-		acumulador = acumulador + numeroIngresado;
-		contador = contador + 1;
-		respuesta = prompt("Agregar otro número: (SI/NO)").toLowerCase();
-	}
-	promedio = acumulador / contador;
+        do {
+            edad = parseInt(prompt("Ingrese edad: (entre 12 y 17)"));
+        } while (edad < 12 || edad > 17);
 
-	document.getElementById("txtIdSuma").value = acumulador;
-	document.getElementById("txtIdPromedio").value = promedio;
+        peso = parseFloat(prompt("Ingrese peso: "));
+
+        altura = parseFloat(prompt("Ingrese altura: "));
+
+        if(flagedadJugadorMasAlto == 0){
+            edadJugadorMasAlto = edad;
+            alturaMayor = altura;
+            flagedadJugadorMasAlto = 1;
+        }
+        else if (alturaMayor < altura) {
+            edadJugadorMasAlto = edad;
+            alturaMayor = altura;
+        }
+
+        if(flagJugadorMenosPesado == 0){
+            nombreJugadorMenosPesado = nombre;
+            pesoJugadorMenosPesado = peso;
+            flagJugadorMenosPesado = 1;
+        }
+        else if (pesoJugadorMenosPesado > peso) {
+            nombreJugadorMenosPesado = nombre;
+            pesoJugadorMenosPesado = peso;
+        }
+
+        pesoDelEquipo += peso;
+        cantidadJugadores++;
+
+        if ((edad > 13 || edad < 15) && (altura > 1.65 || altura < 1.79)) {
+            jugadoresEdad13Y15Altura165Y179++;
+        }
+        
+    }
+
+    promedioPesoEquipo = pesoDelEquipo / cantidadJugadores;
+
+	
 }
--- 8-TPs/12-jsRisingBTL.js
+++ 8-TPs/12-jsRisingBTL.js
@@ -7,13 +7,42 @@ D.	Sueldo bruto, no menor a 8000.
 E.	Número de legajo, numérico de 4 cifras, sin ceros a la izquierda.
 F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.
  */
-function ComenzarIngreso () 
+function ComenzarIngreso() 
 {
- 	//definicion de variables
- 	var edadIngresada;
- 	var sexoIngresado;
- 	var estadoCivilIngresado;
+	var edadIngresada;
+	var sexoIngresado;
+	var estadoCivilIngresado;
+	var sueldoBruto;
+	var numeroLegajo;
+	var nacionalidad;
 
-
-
+	edadIngresada = parseInt(prompt("Ingrese su edad (entre 18 y 90)"));
+	while (edadIngresada < 18 || edadIngresada > 90) {
+	}
+	sexoIngresado = prompt("Ingrese letra correspondiente a sexo: (m) para masculino / (f) para femenino").toLocaleLowerCase();
+	while (sexoIngresado != "m" && sexoIngresado != "f") {
+		sexoIngresado = prompt("Error: Reingrese letra sexo correspondiente: (m) para masculino / (f) para femenino");
+	}
+	estadoCivilIngresado = parseInt(prompt("Ingrese numero correspondiente a estado civil: (1) soltero / (2) casado / (3) divorciado / (4) viudos"));
+	while (estadoCivilIngresado < 1 || estadoCivilIngresado > 4) {
+		estadoCivilIngresado = parseInt(prompt("Error: Reingrese numero correspondiente a estado civi: (1) soltero / (2) casado / (3) divorciado / (4) viudos"));
+	}
+	sueldoBruto = parseInt(prompt("Ingrese sueldo mayor a 8000"));
+	while (sueldoBruto < 8000) {
+		sueldoBruto = parseInt(prompt("Error: Reingrese sueldo mayor a 8000"));
+	}
+	numeroLegajo = parseInt(prompt("Ingrese numero legajo de 4 cifras sin ceros a la izquierda"));
+	while (numeroLegajo < 1000 || numeroLegajo > 9999){
+		numeroLegajo = parseInt(prompt("Error: Reingrese numero legajo valido (desde 1000 hasta 9999)"));
+	}
+	nacionalidad = prompt("Ingrese letra de nacionalidad correspondiente: (a) Argentino || (e) Extranjero || (n) Nacionalizado").toLocaleLowerCase();
+	while (nacionalidad != "a" && nacionalidad != "e" && nacionalidad !="n") {
+		nacionalidad = prompt("Error: Reingrese letra de nacionalidad correspondiente: (a) Argentino || (e) Extranjero || (n) Nacionalizado")
+	} 
+	document.getElementById("txtIdEdad").value = edadIngresada;
+	document.getElementById("txtIdSexo").value = sexoIngresado;
+	document.getElementById("txtIdEstadoCivil").value = estadoCivilIngresado;
+	document.getElementById("txtIdSueldo").value = sueldoBruto;
+	document.getElementById("txtIdLegajo").value = numeroLegajo;
+	document.getElementById("txtIdNacionalidad").value = nacionalidad;
 }
